a4c62ae1c34efeeaf2b0bf568d92485b64d56291
Greg Brougham <greg.brougham@gmail.com>
2023-10-26 09:47:46 +0100

    Added labels for a number of fields

--- src/frontend/utils/fields-definition.js
+++ src/frontend/utils/fields-definition.js
@@ -2,35 +2,44 @@ export const CONFIG_FILE_PASSWORD_KEY = 'configPass'
 
 const fieldsDefinition = {
   'name': {
+    label: 'Name:',
     type: 'text',
   },
   'desc': {
+    label: 'Description:',
     type: 'text',
   },
   'enabled': {
+    label: 'Enabled:',
     type: 'boolean',
   },
   'hapi': {
-    label: 'HAPi (VPN)',
+    label: 'HAPi (VPN):',
     type: 'boolean',
   },
   'dns': {
+    label: 'DNS addr:',
     type: 'text',
   },
   'port': {
+    label: 'Port addr:',
     type: 'text',
   },
   'device': {
+    label: 'Device type:',
     type: 'select',
     values: ['generic', 'loxone', 'eibport']
   },
   'phyAddr': {
+    label: 'Physical addr:',
     type: 'text',
   },
   'logging': {
+    label: 'Logging:',
     type: 'text',
   },
   'config': {
+    label: 'Configuration:',
     type: 'file',
   },
   [CONFIG_FILE_PASSWORD_KEY]: {
